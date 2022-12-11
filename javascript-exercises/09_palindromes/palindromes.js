938248be8ae7d08d3d772cefeae65be70f334c25
regutierrez <11924623+regutierrez@users.noreply.github.com>
2022-12-11 17:20:03 +0800

    javascript exercises: finish exer09

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let str_new = str.replace(/[^a-zA-Z0-9]/g, ""); 
+    let strLowerCase = str_new.toLowerCase(); 
 
+    let strReverse = strLowerCase.split("").reverse().join("");
+
+    if (strReverse == strLowerCase) return true;
+    return false;
 };
 
 // Do not edit below this line
